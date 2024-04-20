c6e2613e8185fec61e1d584163737c2d82236365
Tayyar Görkem Sayer <tsayer@fivestonebv.com>
2024-04-20 23:28:09 +0300

    Login created

--- app.py
+++ app.py
@@ -74,47 +74,43 @@ from streamlit_authenticator.utilities.exceptions import (CredentialsError,
 with open('config.yaml', 'r', encoding='utf-8') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
+
 # Creating the authenticator object
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
+    config['pre-authorized']
 )
 
 # Creating a login widget
 # Creating a login widget in the sidebar
 try:
-    with st.sidebar:
-        with st.expander("Login", expanded=True):
-            authenticator.login()
+    if not st.session_state.get("authentication_status", False):
+        with st.sidebar:
+            with st.expander("Giriş Yap", expanded=False):
+                try:
+                    authenticator.login(max_login_attempts=5)
+                except LoginError as e:
+                    st.error(str(e))
 
-        with st.expander("Reset Password", expanded=False):
-            try:
-                (username_of_forgotten_password,
-                 email_of_forgotten_password,
-                 new_random_password) = authenticator.forgot_password()
-                if username_of_forgotten_password:
-                    st.success('New password sent securely')
-                    # Random password to be transferred to the user securely
-                elif not username_of_forgotten_password:
-                    st.error('Username not found')
-            except ForgotError as e:
-                st.error(e)
-
-
-except LoginError as e:
-    st.sidebar.error(e)
-
+            with st.expander("Kayıt Ol", expanded=False):
+                try:
+                    (email_of_registered_user,
+                     username_of_registered_user,
+                     name_of_registered_user) = authenticator.register_user(pre_authorization=True)
+                    if email_of_registered_user:
+                        st.success('Kullanıcı başarıyla kaydedildi')
+                except RegisterError as e:
+                    st.error(e)
 
     if st.session_state["authentication_status"]:
         authenticator.logout()
-        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.sidebar.write(f'Hoşgeldin *{st.session_state["name"]}*')
 
-
-        st.title('Some content')
         st.header("Eye Authentic Hoşgeldiniz")
-        st.text("Son Güncelleme: 18.04.2024")
+        st.text("Son Güncelleme: 20.04.2024")
 
         st.sidebar.title("Eye Authentic Assistant")
         st.sidebar.subheader("Görselinizin yüklenmesi biraz zaman alabilir. Lütfen bekleyin.")
@@ -281,9 +277,18 @@ except LoginError as e:
 
 
     elif st.session_state["authentication_status"] is False:
-        st.sidebar.error('Username/password is incorrect')
+        st.sidebar.error('Kullanıcı adı/şifre yanlış')
     elif st.session_state["authentication_status"] is None:
-        st.sidebar.warning('Please enter your username and password')
+        st.sidebar.warning('Lütfen sisteme erişebilmek için giriş yapın.')
+
+
+    # Saving config file
+    with open('config.yaml', 'w', encoding='utf-8') as file:
+        yaml.dump(config, file, default_flow_style=False)
+
+
+except CredentialsError as e:
+    st.error(e)
 
 
 
